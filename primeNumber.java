c7dad9eada1edf3e36f72c4f60648bd7219a9b78
Daniel Marchi Silva <daniel.marchi@outlook.com>
2017-08-11 06:28:34 -0300

    Creating project

---
+++ primeNumber.java
@@ -0,0 +1,25 @@
+public class primeNumber {
+
+    
+    public static void main(String[] args) {
+        int i;
+        int n;
+        int qtd=0;
+        boolean prime;
+        
+        
+        for(n=1; n<=100; n++)
+        {  
+            prime=true; //It's prime until proven otherwise.
+ 
+            i=2;
+            do{                
+               if (n%i==0 && n!=i || n==1) prime=false; 
+               i=i+1;       
+            } while (i<n && prime==true);
+        
+            if (prime==false)System.out.println(+n+" prime");
+            else System.out.println(+n+" not prime");            
+        }       
+    }    
+}
